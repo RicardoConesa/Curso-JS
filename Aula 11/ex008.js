d1758e16b7b4d683893c4334aeede9e382b1c902
Ricardo Conesa <ricardo.conesa@gmail.com>
2025-04-17 17:40:34 -0300

    Exercicios JS

---
+++ Aula 11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 70.5
+
+console.log(`velocidade do seu carro é de ${vel} Km/h`)
+
+if(vel >= 60){
+    console.log(`você ultrapassou a cima da velocidade permitida, Você sera multado`)
+}
+//else{
+        console.log(`dirija sempre com cinto de segurança`)
+    //}
---
+++ Aula 11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'BRASIL'
+console.log(`vivendo em ${pais}`)
+if(pais == 'BRASIL')
+    console.log('Você é Brasileiro')
+else
+    console.log('Você é Estrangeito')
